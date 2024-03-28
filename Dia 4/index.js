8e076ac3d7a324b639c9329a21bc4dc8bed42045
Vinicius Vogel <viniciusgabr89@gmail.com>
2024-03-27 22:03:35 -0300

    atividade 1 alt

--- Dia 4/index.js
+++ Dia 4/index.js
@@ -1,15 +1,15 @@
 let fome = prompt("Voce está com fome?")
 let dinheiro = prompt("Voce tem dinheiro?")
-let restaurante = prompt("O restaurante está aberto?")
+let aberto = prompt("O restaurante está aberto?")
 
 if (fome === "nao" || dinheiro === "nao"){
     console.log("Hoje a janta será em casa!")
 }
 
-if (fome === "sim" && dinheiro === "sim" && restaurante === "nao"){
+if (fome === "sim" && dinheiro === "sim" && aberto === "nao"){
     console.log("Peca um delivery!")
 }
 
-if (fome === "sim" && dinheiro === "sim" && restaurante === "sim"){
+if (fome === "sim" && dinheiro === "sim" && aberto === "sim"){
     console.log("Hoje o jantar será no restaurante escolhido! :)")
 }
